8a757580b478bf31715b7896a43bc965001105e5
Tim Cameron Ryan <tim@tim-ryan.com>
2008-04-07 21:59:14 +0000

    Better readme.

---
+++ chowdah/file/FiniteCollection.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Finite Collection Interface
+ *
+ * A Collection is an object which has a finite number children, which can be
+ * retrieved individually via the <kbd>getChild</kbd> method or in an array via
+ * the <kbd>getChildren</kbd> method and its flags.
+ * @package File
+ */
+
+interface FiniteCollection extends Collection {
+	/**
+	 * Get an array of children.
+	 *
+	 * Returns an array of the children of this Collection matching the
+	 * specified flags.
+	 * @param integer The type of children to return. This can be either
+	 * FiniteCollection::CHILD_DOCUMENTS or
+	 * FiniteCollection::CHILD_COLLECTIONS.
+	 */
+	public function getChildren($flags = null);	
+
+	// getChildren flags
+	const CHILD_DOCUMENTS = 1;
+	const CHILD_COLLECTIONS = 2;
+}
+
+?>
