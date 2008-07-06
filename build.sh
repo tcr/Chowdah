9b8a7e1074ad15bf8328206e4ea87873e4354022
Tim Cameron Ryan <tim@tim-ryan.com>
2008-07-06 05:53:48 +0000

    You can't build what don't exist!

--- build.sh
+++ build.sh
@@ -13,9 +13,3 @@ cp -r quiki quiki-$version
 cp -r chowdah/chowdah/* quiki-$version/quiki/.quiki/chowdah
 tar -czf quiki-$version.tar.gz quiki-$version
 rm -rf quiki-$version
-
-# Server
-cp -r server server-$version
-cp -r chowdah/chowdah/* server-$version/server/.server/chowdah
-tar -czf server-$version.tar.gz server-$version
-rm -rf server-$version
